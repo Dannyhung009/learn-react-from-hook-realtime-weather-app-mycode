baf4f380deb8e0b5163069ef1297b989aae3c2ae
danny <76043300+Dannyhung009@users.noreply.github.com>
2024-04-18 12:24:38 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,14 +1,146 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
+import { ReactComponent as DayCloudyIcon } from './images/day-cloudy.svg';
+import { ReactComponent as RainIcon } from './images/rain.svg';
+import { ReactComponent as RefreshIcon } from './images/refresh.svg';
 
-function App() {
-  return (
-      <div className="container">
-        <div className="weather-card">
-          <h1>Weather</h1>
-        </div>
-      </div>
-  );
-}
+import styled from '@emotion/styled'; // STEP 1：載入 emotion 的 styled 套件
+
+// STEP 2：定義帶有 styled 的 component
+const Container = styled.div`
+        background-color: #ededed;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    `;
+
+const WeatherCard = styled.div`
+        position: relative;
+        min-width: 360px;
+        box-shadow: 0 1px 3px 0 #999999;
+        background-color: #f9f9f9;
+        box-sizing: border-box;
+        padding: 30px 15px;
+    `;
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 300;
+  color: #828282;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const DayCloudy = styled(DayCloudyIcon)`
+  flex-basis: 30%;
+`;
+
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: #828282;
+
+  svg {
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+  }
+`;
+
+// STEP 3：把上面定義好的 styled-component 當成元件使用
+// const App = () => {
+//     return (
+//         <Container>
+//             <WeatherCard>
+//                 <Location>台北市</Location>
+//             </WeatherCard>
+//         </Container>
+//     );
+// };
+// export default App;
+
+
+const App = () => {
+    return (
+        <Container>
+            <WeatherCard>
+                <Location>台北市</Location>
+                <Description>多雲時晴</Description>
+                <CurrentWeather>
+                    <Temperature>
+                        23 <Celsius>°C</Celsius>
+                    </Temperature>
+                    <DayCloudy />
+                </CurrentWeather>
+                <AirFlow>
+                    <AirFlowIcon /> 23 m/h
+                </AirFlow>
+                <Rain>
+                    <RainIcon /> 48%
+                </Rain>
+                <Refresh>
+                    最後觀測時間：上午 12:03 <RefreshIcon />
+                </Refresh>
+            </WeatherCard>
+        </Container>
+    );
+};
 
 export default App;
